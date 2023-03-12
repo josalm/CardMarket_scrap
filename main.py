41498255c2623e6233097994fd4178f8275c7a53
up201205741 <up201205741@fc.up.pt>
2023-03-12 02:42:32 +0000

    Now Working

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Hit_Int = 0
 count = 0
 TotalPrice = 0
 DecimalPrice = 0
+LowestPriceCheck = False
 
 root = tk.Tk()
 root.withdraw()
@@ -14,13 +15,17 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 
 print('1) Search Commander Deck')
-print('2) Search Normal Deck - not implemented')
+print('2) Search Commander Deck with lowest price check')
+print('3) Search Normal Deck - not implemented')
 #print('3) Search Commander Deck')
 
 Choice = int(input())
 if Choice == 1 or Choice == 2:
     print('Insert CardMarket user: ')
     UserName = input()  #Username example: CardsCentral, voodoo-pt
+
+if Choice == 2:
+    LowestPriceCheck = True
 
 f = open(file_path,'r')
 for line in f:
@@ -56,7 +61,17 @@ for line in f:
         PriceContentString = Price_content.split(' ')[0]
         PriceContentString = PriceContentString.replace(',','.')
         TotalPrice += float(PriceContentString)
-        print(card + ' ' + Price_content + ' ' + TotalUrl)
+        if not LowestPriceCheck:
+            print(card + ' ' + Price_content + ' ' + TotalUrl)
+        else:
+            PriceCheckUrl = 'https://www.cardmarket.com/en/Magic/Products/Search?idCategory=0&idExpansion=0&searchString=' + card +'&idRarity=0&sortBy=price_asc&perSite=30'
+            page = requests.get(PriceCheckUrl)
+            soup = BeautifulSoup(page.content, "html.parser")
+            
+            LowestPrice = soup.find("div", {"id": "productRow364044"})
+            print('Card: ' + card + '| User Price:  ' + Price_content + '| Lowest Price: ' + LowestPrice + '| Url: ' + TotalUrl)
+            
+            
         count += 1
 
     Hit_Int = 0
